008f3dec944910c5c1f9a0bb2da27645284d7461
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2025-01-13 18:05:20 +0900

    feat: 🎸 break down note.sh into each chapter notes

---
+++ setup.sh
@@ -0,0 +1,25 @@
+$ sudo apt install binutils build-essential golang sysstat python3-matplotlib python3-pil fonts-takao
+ fio qemu-kvm virt-manager libvirt-clients virtinst jq docker.io containerd libvirt-daemon-system
+
+sudo apt install -y binutils build-essential golang sysstat python3-matplotlib python3-pil fonts-takao fio qemu-kvm virt-manager libvirt-clients virtinst jq docker.io containerd libvirt-daemon-system
+
+
+$ sudo adduser `id -un` libvirt
+$ sudo adduser `id -un` libvirt-qemu
+$ sudo adduser `id -un` kvm
+
+ctl c copy
+ctl v paste
+
+https://github.com/satoru-takeuchi/linux-in-practice-2nd/
+
+https://go.dev/doc/install
+
+https://starship.rs/
+
+sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+
+# keyboard mapping
+1234567890-=[];’\,./
+!@#$%^&*()_+{}:”|<>?
