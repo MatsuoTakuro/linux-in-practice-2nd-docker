5fa9d9b64e3703cd5fbdff3fcb84f3f4c1d4ec01
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2025-01-18 11:23:32 +0900

    feat: 🎸 update until ch08

--- linux-in-practice-2nd/08-storage-hierarchy/cache.go
+++ linux-in-practice-2nd/08-storage-hierarchy/cache.go
@@ -24,11 +24,12 @@ import (
 )
 
 const (
-	CACHE_LINE_SIZE = 64
+	CACHE_LINE_SIZE = 64 // bytes
 	// プログラムがうまく動作しなければこの値を変更してください。高速なマシンではアクセス数が足りずに、
 	// とくにバッファサイズが小さいときの値がおかしいことがあります。低速なマシンでは時間がかかりすぎる
 	// ことがあるので値を小さくしてください
-	NACCESS = 128 * 1024 * 1024
+	// N_ACCESS = 128 * 1024 * 1024 // 128M回のアクセス
+	N_ACCESS = 128 * 1024 * 1024 * 10 // 1280M回のアクセス (3分ほどかかる...)
 )
 
 func main() {
@@ -38,23 +39,28 @@ func main() {
 		log.Fatal("openfile()に失敗しました")
 	}
 	defer f.Close()
+	// iの値を2から16まで0.25ずつ増やしていく
 	for i := 2.0; i <= 16.0; i += 0.25 {
-		bufSize := int(math.Pow(2, i)) * 1024
+		bufSize := int(math.Pow(2, i)) * 1024 // KiB (2^i * 1024), total buffer size
+
+		// バッファサイズに相当するメモリ領域を獲得
 		data, err := syscall.Mmap(-1, 0, bufSize, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_ANON|syscall.MAP_PRIVATE)
 		defer syscall.Munmap(data)
 		if err != nil {
 			log.Fatal("mmap()に失敗しました")
 		}
 
-		fmt.Printf("バッファサイズ 2^%.2f(%d) KiB についてのデータを収集中...\n", i, bufSize/1024)
+		fmt.Printf("バッファサイズ 2^%6.2f(%5d) KiB についてのデータを収集中...\n", i, bufSize/1024)
 		start := time.Now()
-		for i := 0; i < NACCESS/(bufSize/CACHE_LINE_SIZE); i++ {
+		// バッファの全キャッシュラインにシーケンシャルにアクセス
+		for i := 0; i < N_ACCESS/(bufSize/CACHE_LINE_SIZE); i++ {
+			// キャッシュラインサイズごとにアクセス
 			for j := 0; j < bufSize; j += CACHE_LINE_SIZE {
 				data[j] = 0
 			}
 		}
 		end := time.Since(start)
-		f.Write([]byte(fmt.Sprintf("%f\t%f\n", i, float64(NACCESS)/float64(end.Nanoseconds()))))
+		f.Write([]byte(fmt.Sprintf("%f\t%f\n", i, float64(N_ACCESS)/float64(end.Nanoseconds()))))
 	}
 	command := exec.Command("./plot-cache.py")
 	out, err := command.Output()
